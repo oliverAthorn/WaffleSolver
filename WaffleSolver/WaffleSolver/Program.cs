3009d03232aac8990c20f07987727efc6b142826
Oliver Athorn <oathorn@tutanota.com>
2022-09-11 14:42:31 +0100

    Simple implementation of Waffle Solver

---
+++ WaffleSolver/WaffleSolver/Program.cs
@@ -0,0 +1,42 @@
+﻿var possibleWords = File.ReadAllLines(@"D:\Desktop\usFiveLetterWords.txt");
+var finalWordList = new List<string>();
+var counter = 0;
+char[] inputLetters = new char[21];
+
+for (int i = 0; i < inputLetters.Length; i++)
+{
+    inputLetters[i] = Console.ReadLine()[0];
+}
+
+foreach (var word in possibleWords)
+{
+    for (int i = 0; i < inputLetters.Length; i++)
+    {
+        var inputChar = inputLetters.ElementAt(i);
+
+        for (int j = 0; j < word.Length; j++)
+        {
+            var wordChar = word.ElementAt(j);
+
+            if (wordChar.Equals(inputChar))
+            {
+                counter++;
+            }
+        }
+    }
+
+    if (counter == 5)
+    {
+        finalWordList.Add(word);
+    }
+
+    else
+    {
+        counter = 0;
+    }
+}
+
+foreach (var word in finalWordList)
+{
+    Console.WriteLine(word);
+}
